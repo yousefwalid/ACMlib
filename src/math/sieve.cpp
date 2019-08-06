17195c3b2a9dd7f50ee7d31e119bf9e9ad7b1ffa
Youssef Walid <yousefwalid98@hotmail.com>
2019-08-06 19:41:18 +0200

    Added Euler's Phi and fixed bugs in Sieve

--- src/math/sieve.cpp
+++ src/math/sieve.cpp
@@ -5,7 +5,7 @@ void Sieve() // O(nlog(logn))
 { 
 	prime[0] = prime[1] = 0;
 	for (int i = 2; i*i <= MAX; i++)
-		if (primes[i])
+		if (prime[i])
 			for (int j = i * 2; j <= MAX; j += i)
-				primes[j] = 0;
-}
+				prime[j] = 0;
+}v
